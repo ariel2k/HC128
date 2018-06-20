d62fbb69d62d210fba83b3c6b5af9cbad0b1273f
Ariel <Ariel@ARIEL-DESKTOP>
2018-06-20 19:56:47 -0300

    x

---
+++ HC128.Desktop/Models/API.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC128.Desktop.Models
+{
+    public static class API
+    {
+        public static async Task<string> GetImageName(string url)
+        {
+            var client = new HttpClient();
+            var path = Path.Combine(url,"/api/Image/Names");
+            HttpResponseMessage response = await client.GetAsync(path);
+            
+        }
+    }
+}
--- HC128.Desktop/frmHC128.cs
+++ HC128.Desktop/frmHC128.cs
@@ -76,7 +76,7 @@ namespace HC128.Desktop
             return img.ToBytes();
         }
 
-        private void Upload(string nameFile, Byte[] bytes)
+        private async Task<> Upload(string nameFile, Byte[] bytes)
         {
             string stringImg = Encoding.UTF8.GetString(bytes);
             ImgAPI imgAPI = new ImgAPI
@@ -84,12 +84,13 @@ namespace HC128.Desktop
                 imageName = nameFile,
                 imageByteArray = stringImg
             };
-            
 
+            var list = await API.GetImageName(txtIPServer.Text);
 
             string caption = "HC-128";
             string message = "Imagen encriptada exitosamente. ";
             MessageBox.Show(this, message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
         }
 
 
